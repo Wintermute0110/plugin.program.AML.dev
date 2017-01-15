a13e93b8d4e3ce0ed2d4fd3fe806378c2a3ecedf
Wintermute0110 <wintermute0110@gmail.com>
2017-01-15 22:04:36 +0900

    Asset rendering working OK.

--- resources/main.py
+++ resources/main.py
@@ -312,7 +312,8 @@ class Main:
     #----------------------------------------------------------------------------------------------
     def _render_machine_parent_list(self, list_name):
         # >> Load main MAME info DB and PClone index
-        MAME_info_dic = fs_load_JSON_file(PATHS.MAIN_DB_PATH.getPath())
+        MAME_db_dic     = fs_load_JSON_file(PATHS.MAIN_DB_PATH.getPath())
+        MAME_assets_dic = fs_load_JSON_file(PATHS.MAIN_ASSETS_DB_PATH.getPath())
         if   list_name == 'Machines':   Machines_PClone_dic = fs_load_JSON_file(PATHS.MACHINES_IDX_PATH.getPath())
         elif list_name == 'NoCoin':     Machines_PClone_dic = fs_load_JSON_file(PATHS.MACHINES_IDX_NOCOIN_PATH.getPath())
         elif list_name == 'Mechanical': Machines_PClone_dic = fs_load_JSON_file(PATHS.MACHINES_IDX_MECHA_PATH.getPath())
@@ -322,8 +323,9 @@ class Main:
         # >> Render parent main list
         self._set_Kodi_all_sorting_methods()
         for parent_name in Machines_PClone_dic:
-            machine = MAME_info_dic[parent_name]
-            self._render_machine_row(parent_name, machine, True, list_name)
+            machine = MAME_db_dic[parent_name]
+            assets  = MAME_assets_dic[parent_name]
+            self._render_machine_row(parent_name, machine, assets, True, list_name)
         xbmcplugin.endOfDirectory(handle = self.addon_handle, succeeded = True, cacheToDisc = False)
 
     #
@@ -354,7 +356,7 @@ class Main:
     # Information and artwork/assets are the same for all machines.
     # URL is different: parent URL leads to clones, clone URL launchs machine.
     #
-    def _render_machine_row(self, machine_name, machine, is_parent_list, list_name = u''):
+    def _render_machine_row(self, machine_name, machine, machine_assets, is_parent_list, list_name = ''):
         display_name = machine['description']
         
         # --- Mark Status ---
@@ -371,13 +373,35 @@ class Main:
         if   machine['driver_status'] == u'imperfect':   display_name += ' [COLOR yellow][Imp][/COLOR]'
         elif machine['driver_status'] == u'preliminary': display_name += ' [COLOR red][Pre][/COLOR]'
 
+        # --- Assets/artwork ---
+        thumb_path      = machine_assets['title']
+        thumb_fanart    = machine_assets['snap']
+        thumb_banner    = machine_assets['marquee']
+        thumb_clearlogo = machine_assets['clearlogo']
+        thumb_poster    = machine_assets['flyer']
+
         # --- Create listitem row ---
-        icon = 'DefaultFolder.png'
-        listitem = xbmcgui.ListItem(display_name, iconImage = icon)
-        ICON_OVERLAY = 6
-        # listitem.setProperty('fanart_image', category_dic['fanart'])
-        listitem.setInfo('video', {'Title'   : display_name,        
-                                   'Overlay' : ICON_OVERLAY } )
+        default_icon = 'DefaultFolder.png'
+        ICON_OVERLAY = 6        
+        listitem = xbmcgui.ListItem(display_name)
+
+        # --- Metadata ---
+        # >> Make all the infotables compatible with Advanced Emulator Launcher
+        listitem.setInfo('video', {'title'   : display_name,            'year'    : machine['year'],
+                                   'genre'   : '',                      'plot'    : '',
+                                   'studio'  : machine['manufacturer'], 'rating'  : '',
+                                   'trailer' : '',                      'overlay' : ICON_OVERLAY })
+        listitem.setProperty('platform', 'MAME')
+
+        # --- Assets ---        
+        # >> AEL custom artwork fields
+        listitem.setArt({'title'     : machine_assets['title'],   'snap'    : machine_assets['snap'],
+                         'boxfront'  : machine_assets['cabinet'], 'boxback' : machine_assets['cpanel'], 
+                         'cartridge' : machine_assets['PCB'],     'flyer'   : machine_assets['flyer'] })
+
+        # >> Kodi official artwork fields
+        listitem.setArt({'icon'   : thumb_path,   'fanart'    : thumb_fanart,
+                         'banner' : thumb_banner, 'clearlogo' : thumb_clearlogo, 'poster' : thumb_poster })
 
         # --- Create context menu ---
         commands = []
