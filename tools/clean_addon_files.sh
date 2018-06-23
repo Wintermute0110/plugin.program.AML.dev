0ceca7cd36d8ed653eada63d09bfa907b7504647
Wintermute0110 <wintermute0110@gmail.com>
2018-06-23 19:48:56 +0900

    Fixed clean script.

--- tools/clean_addon_files.sh
+++ tools/clean_addon_files.sh
@@ -3,6 +3,6 @@
 # Clean uneeded files to pack addon ZIP for release.
 #
 echo "Cleaning pyo files"
-find -name '*.pyo' -print -exec rm '{}' ';'
+find .. -name '*.pyo' -print -exec rm '{}' ';'
 echo "Cleaning pyc files"
-find -name '*.pyc' -print -exec rm '{}' ';'
+find .. -name '*.pyc' -print -exec rm '{}' ';'
