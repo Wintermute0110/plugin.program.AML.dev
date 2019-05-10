06f1d8cbdb0e3a8e11de51e21947e2785fe837f4
Wintermute0110 <wintermute0110@gmail.com>
2019-05-10 21:21:29 +0900

    Bump to version 0.9.11.

--- make_release.py
+++ make_release.py
@@ -33,7 +33,7 @@ import sys
 AML_DEV_ID  = 'plugin.program.AML.dev'
 AML_ID      = 'plugin.program.AML'
 AML_NAME    = 'Advanced MAME Launcher'
-AML_VERSION = '0.9.10'
+AML_VERSION = '0.9.11'
 root_file_list = [
     'addon.py', 'addon.xml',
     'AUTHORS.md', 'changelog.txt', 'LICENSE.txt', 'NOTES.md', 'README.md', 'SKINNING.md',
