4392e1a6a258160e1713baa9050418037fa53e54
Wintermute0110 <wintermute0110@gmail.com>
2020-01-21 13:17:46 +0900

    Fix parsing of History.dat 2.17.

--- resources/mame.py
+++ resources/mame.py
@@ -738,7 +738,8 @@ def mame_load_History_DAT(filename):
     # check if the data is OK before adding it to the index and the DB.
     # 0 -> Looking for '$info=machine_name_1,machine_name_2,' or '$SL_name=item_1,item_2,'
     #      If '$bio' found go to 1.
-    # 1 -> Reading information. If '$end' found go to 0.
+    # 1 -> Reading information. If '$end' found go to 2.
+    # 2 -> Add information to database if no errors. Then go to 0.
     read_status = 0
 
     # Open file
@@ -747,8 +748,6 @@ def mame_load_History_DAT(filename):
     except IOError:
         log_info('mame_load_History_DAT() (IOError) opening "{0}"'.format(filename))
         return (history_idx_dic, history_dic, version_str)
-
-    # Parse file
     for file_line in f:
         line_number += 1
         stripped_line = file_line.strip()
@@ -766,15 +765,17 @@ def mame_load_History_DAT(filename):
             # Machine list line
             # Parses lines like "$info=99lstwar,99lstwara,99lstwarb,"
             # Parses lines like "$info=99lstwar,99lstwara,99lstwarb"
-            # History.dat has syntactic errors like "$dc=,", beware the dog!
-            m = re.search(r'^\$(.+?)=(.+?),?$', line_str)
+            # History.dat has syntactic errors like "$dc=,".
+            # History.dat has syntactic errors like "$megadriv=".
+            m = re.search(r'^\$(.+?)=(.*?),?$', line_str)
             if m:
                 num_header_line += 1
                 list_name = m.group(1)
                 machine_name_raw = m.group(2)
                 # Remove trailing ',' to fix history.dat syntactic errors like
                 # "$snes_bspack=bsfami,,"
-                if machine_name_raw[-1] == ',': machine_name_raw = machine_name_raw[:-1]
+                if len(machine_name_raw) > 1 and machine_name_raw[-1] == ',':
+                    machine_name_raw = machine_name_raw[:-1]
                 # Transform some special list names
                 if list_name in {'info', 'info,megatech', 'info,stv'}: list_name = 'mame'
                 mname_list = machine_name_raw.split(',')
@@ -793,61 +794,84 @@ def mame_load_History_DAT(filename):
                 if bio_str[0] == '\n': bio_str = bio_str[1:]
                 if bio_str[-1] == '\n': bio_str = bio_str[:-1]
 
-                # Clean m_data of bad data due to History.dat syntax errors.
+                # Clean m_data of bad data due to History.dat syntax errors, for example
+                # empty machine names.
+                # clean_m_data = [
+                #     (list_name, [machine_name_1, machine_name_2, ...] ),
+                #     ...,
+                # ]
                 clean_m_data = []
                 for dtuple in m_data:
                     line_num, list_name, mname_list = dtuple
                     # If list_name is empty drop the full line
                     if not list_name: continue
                     # Clean empty machine names.
-                    clean_mname_list = [machine_name for machine_name in mname_list if machine_name]
+                    clean_mname_list = []
+                    for machine_name in mname_list:
+                        # Skip bad/wrong machine names.
+                        if not machine_name: continue
+                        if machine_name == ',': continue
+                        clean_mname_list.append(machine_name)
                     clean_m_data.append((list_name, clean_mname_list))
 
                 # Reset FSM status
-                read_status = 0
+                read_status = 2
                 num_header_line = 0
                 m_data = []
                 info_str_list = []
-
-                # If errors do not insert data in the databases.
-                if len(clean_m_data) == 0 or len(clean_m_data[0][1]) == 0:
-                    log_warning('On History.dat line {:,}'.format(line_number))
-                    log_warning('len(clean_m_data) < 0 or len(clean_m_data[0][1]) < 0')
-                    log_warning('Ignoring entry in History.dat database')
-                    continue
-
-                # The database list name and machine name are the first list and machine in
-                # the list.
-                db_list_name = clean_m_data[0][0]
-                db_machine_name = clean_m_data[0][1][0]
-                if not db_list_name:
-                    log_warning('On History.dat line {:,}'.format(line_number))
-                    log_warning('Empty db_list_name "{}"'.format(db_list_name))
-                if not db_machine_name:
-                    log_warning('On History.dat line {:,}'.format(line_number))
-                    log_warning('Empty db_machine_name "{}"'.format(db_machine_name))
-
-                # Add list and machine names to index database.
-                for dtuple in clean_m_data:
-                    list_name, mname_list = dtuple
-                    if list_name not in history_idx_dic:
-                        history_idx_dic[list_name] = {'name' : list_name, 'machines' : {}}
-                    for machine_name in mname_list:
-                        m_str = misc_build_db_str_3(machine_name, db_list_name, db_machine_name)
-                        history_idx_dic[list_name]['machines'][machine_name] = m_str
-
-                # Add biography string to main database.
-                if db_list_name not in history_dic: history_dic[db_list_name] = {}
-                history_dic[db_list_name][db_machine_name] = bio_str
             else:
                 info_str_list.append(line_str)
+        elif read_status == 2:
+            # Go to state 0 of the FSM.
+            read_status = 0
+
+            # Ignore machine if no valid data at all.
+            if len(clean_m_data) == 0:
+                log_warning('On History.dat line {:,}'.format(line_number))
+                log_warning('clean_m_data is empty.')
+                log_warning('Ignoring entry in History.dat database')
+                continue
+            # Ignore if empty list name.
+            if not clean_m_data[0][0]:
+                log_warning('On History.dat line {:,}'.format(line_number))
+                log_warning('clean_m_data empty list name.')
+                log_warning('Ignoring entry in History.dat database')
+                continue
+            # Ignore if empty machine list.
+            if not clean_m_data[0][1]:
+                log_warning('On History.dat line {:,}'.format(line_number))
+                log_warning('Empty machine name list.')
+                log_warning('db_list_name "{}"'.format(clean_m_data[0][0]))
+                log_warning('Ignoring entry in History.dat database')
+                continue
+            if not clean_m_data[0][1][0]:
+                log_warning('On History.dat line {:,}'.format(line_number))
+                log_warning('Empty machine name first element.')
+                log_warning('db_list_name "{}"'.format(clean_m_data[0][0]))
+                log_warning('Ignoring entry in History.dat database')
+                continue
+            db_list_name = clean_m_data[0][0]
+            db_machine_name = clean_m_data[0][1][0]
+
+            # Add list and machine names to index database.
+            for dtuple in clean_m_data:
+                list_name, machine_name_list = dtuple
+                if list_name not in history_idx_dic:
+                    history_idx_dic[list_name] = {'name' : list_name, 'machines' : {}}
+                for machine_name in machine_name_list:
+                    m_str = misc_build_db_str_3(machine_name, db_list_name, db_machine_name)
+                    history_idx_dic[list_name]['machines'][machine_name] = m_str
+
+            # Add biography string to main database.
+            if db_list_name not in history_dic: history_dic[db_list_name] = {}
+            history_dic[db_list_name][db_machine_name] = bio_str
         else:
             raise TypeError('Wrong read_status = {} (line {:,})'.format(read_status, line_number))
     # Close file
     f.close()
-    log_info('mame_load_History_DAT() Version "{0}"'.format(version_str))
-    log_info('mame_load_History_DAT() Rows in history_idx_dic {0}'.format(len(history_idx_dic)))
-    log_info('mame_load_History_DAT() Rows in history_dic {0}'.format(len(history_dic)))
+    log_info('mame_load_History_DAT() Version "{}"'.format(version_str))
+    log_info('mame_load_History_DAT() Rows in history_idx_dic {}'.format(len(history_idx_dic)))
+    log_info('mame_load_History_DAT() Rows in history_dic {}'.format(len(history_dic)))
 
     return (history_idx_dic, history_dic, version_str)
 
