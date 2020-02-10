2319fafdb587d0193c5f403755dd5cc1a470dc34
Wintermute0110 <wintermute0110@gmail.com>
2020-02-10 20:37:45 +0900

    Bump version to 0.10.0-alpha.

--- make_release.py
+++ make_release.py
@@ -33,7 +33,7 @@ import sys
 AML_DEV_ID  = 'plugin.program.AML.dev'
 AML_ID      = 'plugin.program.AML'
 AML_NAME    = 'Advanced MAME Launcher'
-AML_VERSION = '0.9.12'
+AML_VERSION = '0.10.0'
 root_file_list = [
     'addon.py',
     'addon.xml',
