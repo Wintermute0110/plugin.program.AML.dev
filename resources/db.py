f736e2681fdc1b1d8771587fff725137e7d68e5e
Wintermute0110 <wintermute0110@gmail.com>
2021-06-21 17:12:02 +0900

    Improve function utils_write_JSON_file()

--- resources/db.py
+++ resources/db.py
@@ -1174,10 +1174,8 @@ def db_get_asset_cache_row(cfg, cache_index_dic, catalog_name, category_name):
 # -------------------------------------------------------------------------------------------------
 # Load and save a bunch of JSON files
 # -------------------------------------------------------------------------------------------------
-#
 # Accepts a list of JSON files to be loaded. Displays a progress dialog.
 # Returns a dictionary with the context of the loaded files.
-#
 def db_load_files(db_files):
     log_debug('db_load_files() Loading {} JSON database files...'.format(len(db_files)))
     db_dic = {}
@@ -1192,7 +1190,7 @@ def db_load_files(db_files):
 
     return db_dic
 
-def db_save_files(db_files, json_write_func = utils_write_JSON_file):
+def db_save_files(db_files):
     log_debug('db_save_files() Saving {} JSON database files...'.format(len(db_files)))
     d_text = 'Saving databases...'
     pDialog = KodiProgressDialog()
@@ -1200,7 +1198,7 @@ def db_save_files(db_files, json_write_func = utils_write_JSON_file):
     for f_item in db_files:
         dict_data, db_name, db_path = f_item
         pDialog.updateProgressInc('{}\nDatabase [COLOR orange]{}[/COLOR]'.format(d_text, db_name))
-        json_write_func(db_path, dict_data)
+        utils_write_JSON_file(db_path, dict_data)
     pDialog.endProgress()
 
 # -------------------------------------------------------------------------------------------------
